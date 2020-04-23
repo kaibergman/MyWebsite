1b1a01ca59a59369633ebee66ac8d0a61a9deac0
kaibergman <kaibergman123@gmail.com>
2020-04-23 21:56:36 +0100

    added Projects and about pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import { Button } from 'antd';
+
+import { GithubOutlined } from '@ant-design/icons';
+import { LinkedinOutlined } from '@ant-design/icons';
+import { TrophyOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
+import Projects from './Projects';
+import About from './About';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+
+var colors = ["#05668D", "#028090", "#00A896", "#02C39A", "#F0F3BD"]; //select background colours
+var currentIndex = 0;
+
+setInterval(function () { //background colours
+  document.body.style.cssText = "background-color: " + colors[currentIndex];
+  currentIndex++;
+  if (currentIndex == undefined || currentIndex >= colors.length) {
+    currentIndex = 0;
+  }
+}, 5000);
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div>
+
+
+        <Row type="flex" justify="space-around">
+          <Col span={3}>
+            <Link to="//github.com/kaibergman">
+              <GithubOutlined type="button" onClick="" style={{ fontSize: '40px' }} />
+            </Link>
+          </Col>
+          <Col span={3}>
+
+          </Col>
+          <Col span={3}>
+            <Link to="//linkedin.com/in/kai-bergman">
+              <LinkedinOutlined type="button" style={{ fontSize: '40px' }} />
+            </Link>
+          </Col>
+          <Col span={3}>
+
+          </Col>
+          <Col span={3}>
+            <Link to="./Projects">
+              <TrophyOutlined type="button" style={{ fontSize: '40px' }} />
+            </Link>
+          </Col>
+          <Col span={3}>
+
+          </Col>
+          <Col span={3}>
+            <Link to="./About">
+              <UserOutlined type="button" onClick="" style={{ fontSize: '40px' }} />
+            </Link>
+          </Col>
+
+        </Row>
+      </div>
+
     </div>
+
+
   );
 }
 
