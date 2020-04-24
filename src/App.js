86e2db3b84f64abb6b90dfe0d07f3aa36b33ff2a
kaibergman <kaibergman123@gmail.com>
2020-04-24 15:15:26 +0100

    first live version

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Button } from 'antd';
-
 import { GithubOutlined } from '@ant-design/icons';
 import { LinkedinOutlined } from '@ant-design/icons';
 import { TrophyOutlined } from '@ant-design/icons';
 import { UserOutlined } from '@ant-design/icons';
 import { Row, Col } from 'antd';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import Projects from './Projects';
-import About from './About';
 import { Route, Link } from 'react-router-dom';
-
 import './App.css';
 
-
 const { Header, Footer, Sider, Content } = Layout;
-
 
 var colours = ['#264653', '#2A9D8F', '#E9C46A', 'F4A261', '#E76F51'],
   i = 0;
 
-
-
-
-
-
+document.title = "Kai Bergman";
 
 function App() {
   return (
